527f6262e3fcbd893fdf3e6c5db501dd72ad93fd
Taylor Nation <tnation14@gmail.com>
2017-06-19 09:02:16 -0400

    Add IAM roles for ECS services, add as outputs

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 #
-# IAM resources
+# Container Instance IAM resources
 #
 data "aws_iam_policy_document" "container_instance_ec2_assume_role" {
   statement {
@@ -27,6 +27,56 @@ resource "aws_iam_role_policy_attachment" "ec2_service_role" {
 resource "aws_iam_instance_profile" "container_instance" {
   name = "${aws_iam_role.container_instance_ec2.name}"
   role = "${aws_iam_role.container_instance_ec2.name}"
+}
+
+#
+# ECS Service IAM permissions
+#
+
+data "aws_iam_policy_document" "ecs_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ecs_service_role" {
+  name               = "ecs${var.environment}ServiceRole"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_assume_role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_service_role" {
+  role       = "${aws_iam_role.ecs_service_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+}
+
+data "aws_iam_policy_document" "ecs_autoscale_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["application-autoscaling.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ecs_autoscale_role" {
+  name               = "ecs${var.environment}AutoscaleRole"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_autoscale_assume_role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_service_autoscaling_role" {
+  role       = "${aws_iam_role.ecs_autoscale_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
 }
 
 #
