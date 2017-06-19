53f90a0252b3250c7e2b7a286fb9821390a63e07
Taylor Nation <tnation14@gmail.com>
2017-06-19 09:02:16 -0400

    Add IAM roles for ECS services, add as outputs

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,22 @@ output "container_instance_security_group_id" {
   value = "${aws_security_group.container_instance.id}"
 }
 
-output "container_instance_ecs_for_ec2_service_role" {
+output "container_instance_ecs_for_ec2_service_role_name" {
   value = "${aws_iam_role.container_instance_ec2.name}"
+}
+
+output "ecs_service_role_name" {
+  value = "${aws_iam_role.ecs_service_role.name}"
+}
+
+output "ecs_autoscale_role_name" {
+  value = "${aws_iam_role.ecs_autoscale_role.name}"
+}
+
+output "ecs_service_role_arn" {
+  value = "${aws_iam_role.ecs_service_role.arn}"
+}
+
+output "ecs_autoscale_role_arn" {
+  value = "${aws_iam_role.ecs_autoscale_role.arn}"
 }
