d44651118b7ba863dd4fb02ede6192ceb487a258
Taylor Nation <tnation14@gmail.com>
2017-12-05 09:30:13 -0500

    most_recent argument is unnecessary
    If we filter by `image-id`, there should only be a single item returned.

--- main.tf
+++ main.tf
@@ -144,10 +144,8 @@ data "aws_ami" "ecs_ami" {
 }
 
 data "aws_ami" "user_ami" {
-  count       = "${var.lookup_latest_ami ? 0 : 1}"
-  most_recent = true
-  owners      = ["${var.ami_owners}"]
-
+  count  = "${var.lookup_latest_ami ? 0 : 1}"
+  owners = ["${var.ami_owners}"]
 
   filter {
     name   = "image-id"
