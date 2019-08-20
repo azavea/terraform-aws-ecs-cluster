069109063d582fbcfadad4102f78172d4ed402dc
Pubudu Perera <suharshan22@gmail.com>
2019-08-20 09:40:45 +0800

    Enable passing name for 'ecs_service_role' (#48)
    * Enable setting name for 'ecs_service_role'
    
    * Moved ecs_service_role_name up in variables.tf

--- locals.tf
+++ locals.tf
@@ -3,4 +3,5 @@ locals {
   autoscaling_group_name        = "asg${title(var.environment)}ContainerInstance"
   security_group_name           = "sgContainerInstance"
   ecs_for_ec2_service_role_name = "${var.environment}ContainerInstanceProfile"
+  ecs_service_role_name         = "ecs${title(var.environment)}ServiceRole"
 }
