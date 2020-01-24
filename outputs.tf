29996fd0215649593c71879fb5b96ded906fc77f
Taylor <taylor.nation@linuxacademy.com>
2020-01-24 15:30:02 -0500

    Conditional outputs

--- outputs.tf
+++ outputs.tf
@@ -7,11 +7,11 @@ output "name" {
 }
 
 output "container_instance_security_group_id" {
-  value = aws_security_group.container_instance.id
+  value = join("", aws_security_group.container_instance[*].id)
 }
 
 output "container_instance_ecs_for_ec2_service_role_name" {
-  value = aws_iam_role.container_instance_ec2.name
+  value = join("", aws_iam_role.container_instance_ec2[*].name)
 }
 
 output "ecs_service_role_name" {
@@ -19,14 +19,14 @@ output "ecs_service_role_name" {
 }
 
 output "container_instance_autoscaling_group_name" {
-  value = aws_autoscaling_group.container_instance.name
+  value = join("", aws_autoscaling_group.container_instance[*].name)
 }
 
 output "ecs_service_role_arn" {
-  value = aws_iam_role.ecs_service_role.arn
+  value = join("", aws_iam_role.ecs_service_role[*].arn)
 }
 
 output "container_instance_ecs_for_ec2_service_role_arn" {
-  value = aws_iam_role.container_instance_ec2.arn
+  value = join("", aws_iam_role.container_instance_ec2[*].arn)
 }
 
